95140a2c31d4b9632e6d796dd7d60e406e36a8fa
Nihal Waseem <nihalwaseem76@gmail.com>
2025-03-18 16:39:51 +0530

    deployed backend code and added deployed backend url

--- Client/src/api.js
+++ Client/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://social-media-backend-kq4l.onrender.com/api";
+
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
